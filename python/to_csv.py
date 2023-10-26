1949368c421ecff83ae424def5b83f4791e860a7
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-10-26 14:28:16 -0500

    Verify that each year uses the same column names.
    Change a print strategy to a try-except-raise strategy
    
      for detecting problems in trimming header.

--- python/to_csv.py
+++ python/to_csv.py
@@ -21,26 +21,38 @@ assert ( mk_sheet_names () ==
          [ sheet_year_preface + str(n)
            for n in year_range ] )
 
-def trim_header ( df : pd.DataFrame ) -> pd.DataFrame:
+def trim_header ( year : int,
+                  df : pd.DataFrame ) -> pd.DataFrame:
   """If CONCEPTO is in row 3 or 4 of column 0, drop every row before it.
   Otherwise fail."""
+  try:
   for row in [3,4]:
     if str(df.iloc[row,0]).strip() == "CONCEPTO":
       df = df.iloc[row:,:]
   assert str(df.iloc[0,0]).strip() == "CONCEPTO"
   df.columns = df.iloc[0] . apply ( str.strip )
   df = df[1:]
+  except Exception:
+    raise ValueError ( "trim_header confused at year " + str(year) )
   return df
 
 def mk_pgn_years () -> Dict [ int, pd.DataFrame ]:
   acc = {}
   for year in year_range:
-    print("loading " + str(year))
     df = trim_header (
-      pd.read_excel (
+      year = year,
+      df = pd.read_excel (
         io         = input_file,
         sheet_name = sheet_year_preface + str(year) ) )
     acc [year] = df
   return acc
 
+def verify_column_names ( dfs : Dict [ str, pd.DataFrame ] ):
+  c = dfs [year_min] . columns
+  for k in dfs.keys():
+    if not (dfs[k].columns == c) . all():
+      raise ValueError ( "Column names for year " + str(k) +
+                         " do not match those of the first year." )
+
 dfs = mk_pgn_years ()
+verify_column_names ( dfs )
