fa435168a6ab53767b6eafd62bd192e08844e6b6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-10-27 15:56:18 -0500

    +def mk_homologacion_years () -> Dict [ int, pd.DataFrame ]: +  """This extracts the raw tables from the two tabs of `Homologacion.xlsx`. +     Those tables mix sectors and entities."""

--- python/extract_excel/homologacion.py
+++ python/extract_excel/homologacion.py
@@ -24,3 +24,22 @@ assert (
     sheet_year_preface = sheet_year_preface, )
   == [ sheet_year_preface + str(n)
        for n in year_range ] )
+
+def mk_homologacion_years () -> Dict [ int, pd.DataFrame ]:
+  """This extracts the raw tables from the two tabs of `Homologacion.xlsx`.
+Those tables mix sectors and entities."""
+  tabs = {}
+  for year in year_range:
+    df = pd.read_excel (
+      io         = input_file,
+      sheet_name = sheet_year_preface + str(year),
+      usecols    = range(0,5), )
+    df ["year"] = year
+    tabs[year] = df
+    assert ( # PITFALL: A bit tricky. This assertion depends on two facts:
+             # That the first year extracted is `year_min`,
+             # and that the assignment to `tabs[year]` precedes the assertion.
+      ( tabs[year].columns
+        == tabs[year_min].columns )
+      . all () )
+  return tabs
