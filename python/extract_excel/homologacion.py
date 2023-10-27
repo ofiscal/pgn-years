a96e01ef497dba19f2c0ad02803c78532822afcc
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-10-27 16:04:30 -0500

    rename columns

--- python/extract_excel/homologacion.py
+++ python/extract_excel/homologacion.py
@@ -30,10 +30,17 @@ def mk_homologacion_years () -> Dict [ int, pd.DataFrame ]:
 Those tables mix sectors and entities."""
   tabs = {}
   for year in year_range:
-    df = pd.read_excel (
-      io         = input_file,
-      sheet_name = sheet_year_preface + str(year),
-      usecols    = range(0,5), )
+    df = (
+      pd.read_excel (
+        io         = input_file,
+        sheet_name = sheet_year_preface + str(year),
+        usecols    = range(0,5), )
+      . rename ( columns = { "Sección"         : "sector",
+                             "Sector"          : "sector name",
+                             "Sección entidad" : "entity",
+                             "Entidad"         : "entity name",
+                             "Deleted"         : "deleted",
+                            } ) )
     df ["year"] = year
     tabs[year] = df
     assert ( # PITFALL: A bit tricky. This assertion depends on two facts:
