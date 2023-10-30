dd3ac48eafbe19a58d28234f97bb7b4280a90783
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-10-30 17:17:09 -0500

    Functionalize some code that was top-level in main.py
      It is now th function `verify_string_matches ( gastos )`.
    
    Drop redundant total rows
    
      totals of debt paid, and the grand total ("total general")

--- python/extract_excel/gastos.py
+++ python/extract_excel/gastos.py
@@ -1,9 +1,19 @@
 # PURPOSE: Read, format data from the `gastos` tabs of
 # `input/Ley_PGN_2013-2022.xlsx`.
+#
+# THE "LEVEL" CONCEPT:
+# The original data conflates three kinds of objects of interest as rows:
+# an agency's spending in one of the three spending categories,
+# an agency, and a sector (which contains agencies).
+# (It also conflates some totals among those rows,
+# which we don't care about.)
+# Of those objects we care about,
+# the spending items what I'm calling are level 0,
+# agencies level 1, and sectors level 2.
 
 import pandas as pd
 import re
-from   typing import Dict, List, Set
+from   typing import Dict, List, Set, Tuple
 #
 import python.extract_excel.lib as lib
 
@@ -72,7 +82,51 @@ def collect_pgn_years ( dfs : Dict [ int, pd.DataFrame ]
                       axis = "rows" )
   return acc
 
-def raw_trimmed_unified_data () -> pd.DataFrame:
+matches_to_level_0_items : List [ Tuple [ str,
+                                          List [ str ] ] ] = [
+  ( "servicio.*deuda", [
+    "SERVICIO DE LA DEUDA PUBLICA NACIONAL",     # to drop (see drop_totals)
+    "SERVICIO DE LA DEUDA PÚBLICA NACIONAL",     # to drop (see drop_totals)
+    "Servicio de la Deuda",               ] ),   # to keep
+  ( "inversi.n",       [ "Inversión"      ] ),   # to keep
+  ( "funcionamiento",  [ "Funcionamiento" ] ), ] # to keep
+
+matches_to_total_general : List [ Tuple [ str,
+                                          List [ str ] ] ] = [
+  ( "total.*general",  [ "Total general"  ] ) ]  # to drop (see drop_totals)
+
+def verify_string_matches ( gastos : pd.DataFrame ):
+  """This is a safety harness, required because the government
+might change the names of sectors or entities in the future,
+such that one of them matches one of these regexes.
+That would lead to our treating it the wrong way.
+To avoid such a silent error creeping in via future changes to the data,
+this verifies that the only regex matches are of the kind we intend."""
+  for (regex, matches) in [ *matches_to_level_0_items,
+                            *matches_to_total_general ]:
+    if not ( pd.Series ( matches )
+             . equals (
+               pd.Series ( gastos [ "name" ]
+                           [ gastos["name"]
+                             . str.match ( ".*" + regex + ".*",
+                                           case = False ) ]
+                           . unique() )
+               . sort_values ()
+               . reset_index ( drop=True ) ) ):
+      raise ValueError ( "verify_string_matches(): "
+                         + "Unexpected matches found to the regex \""
+                         + regex + "\"." )
+
+# TODO: We could keep this information instead,
+# using it as a check on the other information.
+def drop_totals ( gastos : pd.DataFrame ) -> pd.DataFrame:
+  return gastos [
+    ( ~ gastos [ "name" ] . str.match (
+      "Total general" ) ) &
+    ( ~ gastos [ "name" ] . str.match (
+      "SERVICIO DE LA DEUDA P.BLICA NACIONAL" ) ) ]
+
+def mk_gastos () -> pd.DataFrame:
   """Yields a data set with year, name, and COP value.
 PITFALL: The `name` field is still raw --
 it mixes sectors, entities and the three kinds of spending
@@ -81,3 +135,5 @@ it mixes sectors, entities and the three kinds of spending
     mk_pgn_years ()
   verify_column_names ( dfs )
   gastos =  collect_pgn_years ( dfs ) [["year", "name", "cop"]]
+  verify_string_matches ( gastos )
+  return drop_totals ( gastos )
--- python/main.py
+++ python/main.py
@@ -2,36 +2,12 @@ import pandas as pd
 import re
 from   typing import Dict, List, Set
 #
-import python.extract_excel.gastos        as gastos
+import python.extract_excel.gastos        as excel_gastos
 import python.extract_excel.homologacion  as homologacion
 
 
-gastos   = gastos . raw_trimmed_unified_data ()
-tabs     = homologacion.mk_homologacion_years ()
-sectors  = homologacion.extract_sectors_or_entities ( tabs, "sector" )
-entities = homologacion.extract_sectors_or_entities ( tabs, "entity" )
+gastos   = excel_gastos . mk_gastos ()
+tabs     = homologacion . mk_homologacion_years ()
+sectors  = homologacion . extract_sectors_or_entities ( tabs, "sector" )
+entities = homologacion . extract_sectors_or_entities ( tabs, "entity" )
 del(tabs)
-
-for (term, matches) in [
-    # This is a safety harness, required because the government
-    # might change the names of sectors or entities in the future,
-    # such that one of them matches one of these regexes.
-    # That would lead to our treating it the wrong way.
-    # To avoid such a silent error creeping in via future changes to the data,
-    # this verifies that the only regex matches are of the kind we intend.
-    ( "servicio.*deuda", [ "SERVICIO DE LA DEUDA PUBLICA NACIONAL",
-                           "SERVICIO DE LA DEUDA PÚBLICA NACIONAL",
-                           "Servicio de la Deuda", ] ),
-    ( "inversi.n",       [ "Inversión" ] ),
-    ( "funcionamiento",  [ "Funcionamiento" ] ),
-    ( "total.*general",  ["Total general"] ) ]:
-  assert (
-    pd.Series ( matches )
-    . equals (
-      pd.Series ( gastos [ "name" ]
-                  [ gastos["name"]
-                    . str.match ( ".*" + term + ".*",
-                                  case = False ) ]
-                  . unique() )
-      . sort_values ()
-      . reset_index ( drop=True ) ) )
