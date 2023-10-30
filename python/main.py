88b4e4e46d2706880add29aaa5bcbca7e95ef93e
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-10-30 16:10:11 -0500

    nothing (comments, layout)

--- python/main.py
+++ python/main.py
@@ -13,22 +13,18 @@ entities = homologacion.extract_sectors_or_entities ( tabs, "entity" )
 del(tabs)
 
 for (term, matches) in [
-    # This is a safety harness, because the government might
-    # change the names of sectors or entities in the future,
+    # This is a safety harness, required because the government
+    # might change the names of sectors or entities in the future,
     # such that one of them matches one of these regexes.
     # That would lead to our treating it the wrong way.
     # To avoid such a silent error creeping in via future changes to the data,
     # this verifies that the only regex matches are of the kind we intend.
-    ( "servicio.*deuda",
-      [ "SERVICIO DE LA DEUDA PUBLICA NACIONAL",
-        "SERVICIO DE LA DEUDA PÚBLICA NACIONAL",
-        "Servicio de la Deuda", ] ),
-    ( "inversi.n",
-      [ "Inversión" ] ),
-    ( "funcionamiento",
-      [ "Funcionamiento" ] ),
-    ( "total.*general",
-      ["Total general"] ) ]:
+    ( "servicio.*deuda", [ "SERVICIO DE LA DEUDA PUBLICA NACIONAL",
+                           "SERVICIO DE LA DEUDA PÚBLICA NACIONAL",
+                           "Servicio de la Deuda", ] ),
+    ( "inversi.n",       [ "Inversión" ] ),
+    ( "funcionamiento",  [ "Funcionamiento" ] ),
+    ( "total.*general",  ["Total general"] ) ]:
   assert (
     pd.Series ( matches )
     . equals (
